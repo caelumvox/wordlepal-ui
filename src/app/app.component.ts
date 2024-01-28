96b407c9214bf9f89552ebc5d801f703a4b518dc
Matthew Miling <mattmiling@gmail.com>
2024-01-27 21:35:45 -0500

    Added menubar, fixed hyperlinks, fixed issues with submissions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule} from '@angular/material/menu';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule, 
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
     MatNativeDateModule,
+    MatToolbarModule,
     RouterOutlet
   ],
   templateUrl: './app.component.html',
--- src/app/solution-add/solution-add.component.ts
+++ src/app/solution-add/solution-add.component.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import {provideMomentDateAdapter} from '@angular/material-moment-adapter';
+import moment from 'moment';
 import { WordlepalApiService } from '../wordlepal-api.service';
 
 export const WORDLEPAL_DATE_FORMAT = {
@@ -36,17 +37,22 @@ export const WORDLEPAL_DATE_FORMAT = {
   styleUrl: './solution-add.component.css'
 })
 export class SolutionAddComponent {
-  date: Date = new Date();
+  date: moment.Moment;
   words: string = '';
 
   constructor(private wordlepalApiService: WordlepalApiService) {
+    this.date = moment();
   }
 
   ngOnInit() {
   }
 
   submitSolution() {
-    const dateValue = this.date.getFullYear()*10000 + (this.date.getMonth()+1)*100 + this.date.getDate();
+    if (!this.words) {
+      console.log('No words entered. Not submitting.');
+      return;
+    }
+    const dateValue = this.date.year()*10000 + (this.date.month()+1)*100 + this.date.date();
     const wordList = this.words.split(',');
     const solution = {
       date: dateValue,
